f9385f34c37cf7344aa9768277a9328a6b9525af
Yejining <kimyejin.kr@gmail.com>
2023-05-25 22:15:51 +0900

    feat: show private icon in travel list view

--- src/components/travel/item/TravelListItem.vue
+++ src/components/travel/item/TravelListItem.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="shadow-lg border-radius-lg mb-4">
     <div class="p-3 card-body">
-      <div class="d-flex" style="margin-bottom: 12px">
+      <div class="d-flex user-wrap" style="margin-bottom: 12px">
         <div class="border-radius-lg">
           <img :src="thumbnail" alt="profile_image" class="shadow-sm w-100" />
+          <div class="col-md-4 user-text custom-private" v-if="isPrivate">
+            <img src="@/assets/svg/computing-cloud-svgrepo-com.svg" alt="Image placeholder" />
+          </div>
         </div>
       </div>
-      <div :class="contentClass">
+      <div>
         <div class="numbers">
           <p class="mb-1 text-sm">{{ travel.duration }}</p>
-          <h5 class="font-weight-bolder" :class="valueColor">{{ travel.title }}</h5>
+          <h5 class="font-weight-bolder">{{ travel.title }}</h5>
           {{ travel.description }}
         </div>
       </div>
@@ -35,6 +38,11 @@ export default {
       this.thumbnail = this.travel.thumbnail;
     }
   },
+  computed: {
+    isPrivate() {
+      return this.travel.travelStatus === "PRIVATE";
+    }
+  },
 };
 </script>
 
@@ -46,5 +54,33 @@ export default {
 
 .cropping img {
   max-height: initial;
+}
+
+.user-wrap {
+  width: 100%;
+  margin: 10px auto;
+  position: relative;
+}
+.user-wrap img {
+  width: 100%;
+  vertical-align: middle;
+}
+.user-text {
+  position: absolute;
+  text-align: right;
+  right: 0;
+  bottom: 0;
+  margin-right: 10px;
+}
+
+.custom-private {
+  position: absolute;
+  margin: 1rem;
+  top: 0;
+  right: 0;
+  width: 2.5rem;
+  height: 2.5rem;
+  border: none;
+  border-radius: inherit;
 }
 </style>
